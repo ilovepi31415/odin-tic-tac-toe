e82a194e0768bd2fdd1601b79d2afdd5fe290adb
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2025-04-02 12:12:09 -0400

    Allow placing markers on board
    Created players can now place their markers on the
    board, with a verification to make sure the spot
    isn't already taken

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function Gameboard() {
     // prep board
     let markers = [' ', 'X', 'O'];
     let gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
     const display = () => { /* display the board */
         for (row = 0; row < 3; row++) {
             let line = '';
@@ -12,32 +13,61 @@ function Gameboard() {
             console.log(line)
             if (row < 2) {console.log('-----------')}
         }
+        console.log(''); // Adds blank line for better spacing
     };
+
+    const setMarker = (index, symbol) => {
+        if (index > 0) { // Make sure the blank symbol isn't changed
+            markers[index] = symbol;
+        }
+    }
+
+    const isLegalMove = (move) => { /* verify the move as legal */ 
+        return gameState[move - 1] == 0;
+    };
+
+    const place = (move, player) => { /* place the player's marker in the right place */
+        gameState[move - 1] = player;
+    };
+    const hasWon = (player) => { /* check if the given player has won */ };
 
     return {
         display,
+        setMarker,
+        isLegalMove,
+        place,
+        hasWon,
     };
 }
 
 function Player(name, marker) { 
     return {
         name,
-        marker
+        marker,
     };
 }
 
 function Game() {
     let turn = 1;
+    const getMove = (board, player) => {
+        let move;
+        do {
+            move = prompt(`What move are you making, ${player.name}?`);   
+        } while (!board.isLegalMove(move));
+        board.place(move, player.marker);
+    }
 
-    const verify = (move) => { /* verify the move as legal */ };
-    const place = (move, marker) => { /* place the players marker in the right place */ };
-    
     return {
-        verify,
-        place,
+       getMove, 
     }
 }
 
 const game = Game();
 const board = Gameboard();
+const player1 = Player('Alice', 1);
+const player2 = Player('Bob', 2);
+board.display();
+game.getMove(board, player1);
+board.display();
+game.getMove(board, player2);
 board.display();
